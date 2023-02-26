4038b3bdb2b2b9b1776e2d25280a6e807953168e
Robert Mikhailovskiy <kazimir32282@gmail.com>
2023-02-26 12:04:02 +0100

    Add coroutinestart project with callbacks

---
+++ coroutinestart/app/src/main/java/com/example/coroutinestart/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.coroutinestart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.example.coroutinestart.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.buttonLoad.setOnClickListener {
+            loadData()
+        }
+    }
+
+    private fun loadData() {
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        loadCity {
+            binding.tvLocation.text = it
+            loadTemperature(it) { t ->
+                binding.tvTemperature.text = t.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+
+        }
+    }
+
+    private fun loadCity(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            callback("Moscow")
+        }
+    }
+
+    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+        Toast.makeText(
+            this,
+            getString(R.string.loading_temperature_toast, city),
+            Toast.LENGTH_SHORT
+        ).show()
+        Thread.sleep(5000)
+        callback(17)
+    }
+}
