19293eb17b302d486c814e75c5749d8282566a6c
Robert Mikhailovskiy <kazimir32282@gmail.com>
2023-03-12 21:06:28 +0100

    Fix bottom navigation

--- cartandnotes/app/src/main/java/com/example/cart_and_notes/presentation/view/main/MainActivity.kt
+++ cartandnotes/app/src/main/java/com/example/cart_and_notes/presentation/view/main/MainActivity.kt
@@ -8,10 +8,12 @@ import com.example.cart_and_notes.databinding.ActivityMainBinding
 import com.example.cart_and_notes.presentation.view.FragmentManager
 
 // TODO: use use-cases
-// TODO: set bottom navigation selected item to the correct one after adding new item
+// TODO: move add button to action bar
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var currentTab = R.id.notes
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,16 +24,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initBottomNavigation() {
-        binding.bottomNav.selectedItemId = R.id.notes
-        FragmentManager.setFragment(NoteFragment.newInstance(), this)
         binding.bottomNav.setOnItemSelectedListener {
             when(it.itemId) {
                 R.id.setting -> Log.d("BOTTOM_NAV", "Clicked on settings")
-                R.id.notes -> FragmentManager.setFragment(NoteFragment.newInstance(), this)
-                R.id.cart -> FragmentManager.setFragment(CartFragment.newInstance(), this)
-                R.id.new_item -> FragmentManager.currentFragment?.onClickNew()
+                R.id.notes -> {
+                    FragmentManager.setFragment(NoteFragment.newInstance(), this)
+                    currentTab = R.id.notes
+                }
+                R.id.cart -> {
+                    FragmentManager.setFragment(CartFragment.newInstance(), this)
+                    currentTab = R.id.cart
+                }
+                R.id.new_item -> {
+                    FragmentManager.currentFragment?.onClickNew()
+                }
             }
             true
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.bottomNav.selectedItemId = currentTab
     }
 }
